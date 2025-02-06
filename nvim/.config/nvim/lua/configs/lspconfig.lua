f9bfbabba5d943fd4294d51bfd94f2c977dd306b
Master.G <master-g@users.noreply.github.com>
2025-02-06 14:45:25 +0800

    feat: nvim, kitty

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "html", "cssls", "clangd" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -15,6 +15,29 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+-- lspconfig.rust_analyzer.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+--   ft = { "rust" },
+--   root_dir = lspconfig.util.root_pattern("Cargo.toml", "rust-project.json"),
+--   settings = {
+--     ["rust-analyzer"] = {
+--       assist = {
+--         importGranularity = "module",
+--         importPrefix = "by_self",
+--       },
+--       cargo = {
+--         allFeatures = true,
+--         loadOutDirsFromCheck = true,
+--       },
+--       procMacro = {
+--         enable = true,
+--       },
+--     },
+--   },
+-- }
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,25 +1,133 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		-- event = 'BufWritePre', -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+
+	-- Mason
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"lua-language-server",
+				"stylua",
+
+				-- web dev
+				"html-lsp",
+				"css-lsp",
+				"prettier",
+
+				-- rust
+				"rust-analyzer",
+			},
+		},
+	},
+
+	-- Tree sitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				-- defaults
+				"vim",
+				"lua",
+				"vimdoc",
+				"html",
+				"css",
+
+				-- rust
+				"rust",
+				"toml",
+
+				-- work
+				"java",
+				"javascript",
+				"kotlin",
+				"python",
+				"tsx",
+				"typescript",
+
+				-- low level
+				"c",
+				"cpp",
+			},
+		},
+	},
+
+	-- Copilot
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = {
+					auto_trigger = true,
+					debounce = 100,
+					keymap = {
+						accept = "<C-l>",
+					},
+				},
+				filetypes = {
+					css = true,
+					eruby = true,
+					go = true,
+					html = true,
+					javascript = true,
+					lua = true,
+					php = true,
+					ruby = true,
+					rust = true,
+					typescript = true,
+					vue = true,
+					["*"] = false,
+				},
+				server_opts_overrides = {},
+			})
+		end,
+	},
+
+	-- Rust
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
   {
-    "neovim/nvim-lspconfig",
+    "mrcjkb/rustaceanvim",
+    version = "^5",
+    ft = "rust",
+    dependencies = "neovim/nvim-lspconfig",
     config = function()
-      require "configs.lspconfig"
+      require("configs.rustaceanvim")
     end,
   },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "saecki/crates.nvim",
+    ft = {"rust", "toml"},
+    config = function(_, opts)
+      local crates = require("crates")
+      crates.setup(opts)
+      crates.show()
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      table.insert(opts.sources, {name="crates"})
+    end,
+  }
 }
