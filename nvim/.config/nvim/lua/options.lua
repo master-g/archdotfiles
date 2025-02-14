dcc9bae583a02dc752b5060cd75d6e520d589bab
mg <masterg@yeah.net>
2025-02-14 11:11:36 +0800

    feat: nvim

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -9,6 +9,6 @@ o.cursorlineopt ='both' -- to enable cursorline!
 vim.lsp.inlay_hint.enable(true)
 
 -- gui font
-vim.o.guifont = "MonoLisa Variable:h12"
+-- vim.o.guifont = "MonoLisa Variable:h12"
 -- neovide
 vim.g.neovide_cursor_vfx_mode = "railgun"
