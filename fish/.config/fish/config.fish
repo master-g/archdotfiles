d19661b91f5cb276dfd70474702b04e9898ba7db
mg <masterg@yeah.net>
2025-02-18 23:55:32 +0800

    feat: add conda to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -82,8 +82,43 @@ function fpk
   end
 end
 
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+set -gx CRYPTOGRAPHY_OPENSSL_NO_LEGACY 1
+set -l conda_paths \
+  "$HOME/miniconda3" \
+  "$HOME/anaconda3" \
+  "/opt/miniconda3" \
+  "/opt/anaconda3" \
+  "/usr/local/miniconda3" \
+  "/usr/local/anaconda3"
+
+set -l found_conda_path
+for path in $conda_paths
+  if test -f "$path/bin/conda"
+    set found_conda_path "$path"
+    break
+  end
+end
+
+if set -q found_conda_path
+    eval "$found_conda_path/bin/conda" "shell.fish" "hook" $argv | source
+
+    if not functions -q conda
+      if test -f "$found_conda_path/etc/fish/conf.d/conda.fish"
+        source "$found_conda_path/etc/fish/conf.d/conda.fish"
+      else
+        set -gx PATH "$found_conda_path/bin" $PATH
+      end
+    end
+else
+  echo "Conda not found in common locations. If installed elsewhere, add to PATH manually."
+end
+# <<< conda initialize <<<
+
 # make it work in UTM vm
 set -gx MESA_GL_VERSION_OVERRIDE 4.5
 set -gx MESA_GLSL_VERSION_OVERRIDE 450
 
 starship init fish | source
+
