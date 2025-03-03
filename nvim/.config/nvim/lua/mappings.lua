a21a98f2d515ddd95c72fdfde99d10d892a3c951
Master.G <master-g@users.noreply.github.com>
2025-03-03 17:48:25 +0800

    feat: rustlsp code action mapping

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -32,3 +32,4 @@ map("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugge
 
 -- rustaceanvim
 map("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
+map("n", "<M-a>", "<cmd>lua vim.cmd('RustLsp codeAction')<CR>", { desc = "RustLsp code action" })
