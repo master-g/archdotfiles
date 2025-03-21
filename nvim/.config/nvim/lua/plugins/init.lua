600381f8fe7f124923d05fc01625280648479d14
Master.G <master-g@users.noreply.github.com>
2025-03-21 19:53:12 +0800

    feat(nvim): copilot chat command and zh-cn prompts

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -120,7 +120,7 @@ return {
 					auto_trigger = true,
 					debounce = 100,
 					keymap = {
-						accept = "<C-l>",
+						accept = "<Tab>",
 					},
 				},
 				filetypes = {
@@ -143,13 +143,44 @@ return {
 	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
+		cmd = "CopilotChat",
 		dependencies = {
 			{ "zbirenbaum/copilot.lua" },
 			{ "nvim-lua/plenary.nvim", branch = "master" },
 		},
 		build = "make tiktoken",
-		opts = {},
-		lazy = false,
+		opts = {
+			-- the key mapping will mess up with other plugins
+			-- check [here](https://github.com/CopilotC-Nvim/CopilotChat.nvim/issues/986)
+
+			prompts = {
+				ExplainZh = {
+					prompt = "为所选代码撰写解释说明，采用段落形式的文本描述.",
+					system_prompt = "COPILOT_EXPLAIN",
+				},
+				ReviewZh = {
+					prompt = "审查所选代码",
+					system_prompt = "COPILOT_REVIEW",
+				},
+				FixZh = {
+					prompt = "这段代码存在问题，找出问题所在，并在修复的基础上重写代码。解释出错的地方以及你的修改是如何解决这些问题的。",
+				},
+				OptimizeZh = {
+					prompt = "优化所选代码以提升性能和可读性。解释你的优化策略以及你所做更改的好处。",
+				},
+				DocsZh = {
+					prompt = "为所选代码添加文档注释。",
+				},
+				Tests = {
+					prompt = "请为我的代码生成测试用例。",
+				},
+				Commit = {
+					prompt = "请按照 commitizen 规范为该更改编写提交信息。请确保标题不超过 50 个字符，并在 72 个字符处换行格式化，格式采用 gitcommit 代码块",
+					context = "git:staged",
+				},
+			},
+		},
+		-- lazy = false,
 	},
 
 	--JSONc
